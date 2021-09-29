9cd4a08226f0a3dddbe5f29eb09c457cf26d497f
Simanov <smflsimfali@gmail.com>
2021-09-29 14:41:49 +0300

    repair

--- file_sistem/excel/models.py
+++ file_sistem/excel/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Base(models.Model):
+    name = models.CharField('Название', max_length=255, blank=True, null=True)
+    create = models.DateTimeField('Дата создания', blank=True, null=True, default=now)
+    update = models.DateTimeField('Дата изменения', blank=True, null=True, default=now)
+    _create = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(Base):
+    subName = models.CharField('Бренд', max_length=255, blank=True, null=True, default='Комфорт')
+    path = models.CharField('Путь', max_length=1024, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.subName}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(Base):
+    ekd = models.CharField('Документация конструкторов', max_length=1024, blank=True, null=True)
+    electricPdf = models.CharField('Электрические схемы', max_length=1024, blank=True, null=True)
+    passportPdf = models.CharField('Паспорт', max_length=1024, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Изделие'
+        verbose_name_plural = 'Изделия'
--- file_sistem/excel/views.py
+++ file_sistem/excel/views.py
@@ -1,3 +1,66 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from .utils.excel import get_excel, list_files, read_json
+from django.views.generic.base import View
+from .forms import PathForm
+import json
+from pathlib import Path
+from .fill import test_import
 
-# Create your views here.
+
+def read_excel(request):
+    get_excel()
+    return render(request, 'test.html')
+
+
+def get_path(request):
+    if request.method == 'POST':
+        form = PathForm(request.POST)
+        if form.is_valid():
+            # get_excel(form.cleaned_data)
+            clean = form.cleaned_data
+            path = clean['path']
+            fs = list_files(path)
+
+            # return HttpResponseRedirect('/')
+    else:
+        fs = ''
+        path = ''
+        form = PathForm()
+
+    return render(request, 'path.html', {'form': form, 'files': fs, 'path': path})
+
+
+#
+# def pdfView(request):
+#     filename = 'media/A_EKD_KEV_P2211E_book_A4-A3.pdf'
+#     # filename = filename.encode('utf-8')
+#     with open(settings.BASE_DIR / filename, 'rb') as pdf:
+#         res = HttpResponse(pdf.read(), content_type='application/pdf')
+#         res['Content-Disposition'] = 'inline;filename=test.pdf'
+#         return res
+
+def get_file(request):
+    if request.method == 'POST':
+        # return 'test'
+        data = json.loads(request.body)
+        filename = Path(data['fn'])
+    else:
+        filename = request.GET['fn']
+    # filename = filename.encode('utf-8')
+    with open(filename, 'rb') as pdf:
+        res = HttpResponse(pdf.read(), content_type='application/pdf')
+        res['Content-Disposition'] = 'inline;filename=test.pdf'
+        return res
+
+
+class Test(View):
+    name = 'excel.models.Category'
+    template_name = 'path.html'
+
+    def get(self, request):
+        data = read_json('excel/test.json')
+        [test_import(e) for e in data]
+        # test_import(self.name, data)
+        form = PathForm(request)
